02177062a14c0d88f75037889eb0a94207a74e92
ivonildo-lopes <ivonildolopes.ti@gmail.com>
2017-06-07 09:53:28 -0300

    ATUALIZANDO A FORMA DE PAGAMENTO FIADO

--- src/main/java/com/algaworks/pedidovenda/model/Pedido.java
+++ src/main/java/com/algaworks/pedidovenda/model/Pedido.java
@@ -106,151 +106,6 @@ public class Pedido implements Serializable {
 		return !isNovo();
 	}
 
-	// get and set
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Date getDataCriacao() {
-		return dataCriacao;
-	}
-
-	public void setDataCriacao(Date dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
-
-	public String getObservacao() {
-		return observacao;
-	}
-
-	public void setObservacao(String observacao) {
-		this.observacao = observacao;
-	}
-
-	public Date getDataEntrega() {
-		return dataEntrega;
-	}
-
-	public void setDataEntrega(Date dataEntrega) {
-		this.dataEntrega = dataEntrega;
-	}
-
-	public Date getDataEmissao() {
-		return dataEmissao;
-	}
-
-	public void setDataEmissao(Date dataEmissao) {
-		this.dataEmissao = dataEmissao;
-	}
-
-	public BigDecimal getValorFrete() {
-		return valorFrete;
-	}
-
-	public void setValorFrete(BigDecimal valorFrete) {
-		this.valorFrete = valorFrete;
-	}
-
-	public BigDecimal getValorDesconto() {
-		return valorDesconto;
-	}
-
-	public void setValorDesconto(BigDecimal valorDesconto) {
-		this.valorDesconto = valorDesconto;
-	}
-
-	public BigDecimal getValorTotal() {
-		return valorTotal;
-	}
-
-	public void setValorTotal(BigDecimal valorTotal) {
-		this.valorTotal = valorTotal;
-	}
-
-	public StatusPedido getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusPedido status) {
-		this.status = status;
-	}
-
-	public FormaPagamento getFormaPagamento() {
-		return formaPagamento;
-	}
-
-	public void setFormaPagamento(FormaPagamento formaPagamento) {
-		this.formaPagamento = formaPagamento;
-	}
-
-	public Usuario getVendedor() {
-		return vendedor;
-	}
-
-	public void setVendedor(Usuario vendedor) {
-		this.vendedor = vendedor;
-	}
-
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
-
-	public EnderecoEntrega getEnderecoEntrega() {
-		return enderecoEntrega;
-	}
-
-	public void setEnderecoEntrega(EnderecoEntrega enderecoEntrega) {
-		this.enderecoEntrega = enderecoEntrega;
-	}
-
-	public List<ItemPedido> getItens() {
-		return itens;
-	}
-
-	public void setItens(List<ItemPedido> itens) {
-		this.itens = itens;
-	}
-	
-	public int getNumeroParcela() {
-		return numeroParcela;
-	}
-
-	public void setNumeroParcela(int numeroParcela) {
-		this.numeroParcela = numeroParcela;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pedido other = (Pedido) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
 	public void recalcularValorTotal() {
 		BigDecimal total = BigDecimal.ZERO;
@@ -305,6 +160,11 @@ public class Pedido implements Serializable {
 		}
 		
 	}
+	
+	public void removerItem(ItemPedido item){
+		this.getItens().remove(item);
+		adicionaItemVazio();
+	}
 
 	@Transient
 	public boolean isValorTotalNegativo() {
@@ -358,6 +218,150 @@ public class Pedido implements Serializable {
 	public boolean isNaoEnviavelPorEmail() {
 		return isNovo() || isCancelado();
 	}
+	
+	// get and set
+		public Long getId() {
+			return id;
+		}
 
+		public void setId(Long id) {
+			this.id = id;
+		}
 
+		public Date getDataCriacao() {
+			return dataCriacao;
+		}
+
+		public void setDataCriacao(Date dataCriacao) {
+			this.dataCriacao = dataCriacao;
+		}
+
+		public String getObservacao() {
+			return observacao;
+		}
+
+		public void setObservacao(String observacao) {
+			this.observacao = observacao;
+		}
+
+		public Date getDataEntrega() {
+			return dataEntrega;
+		}
+
+		public void setDataEntrega(Date dataEntrega) {
+			this.dataEntrega = dataEntrega;
+		}
+
+		public Date getDataEmissao() {
+			return dataEmissao;
+		}
+
+		public void setDataEmissao(Date dataEmissao) {
+			this.dataEmissao = dataEmissao;
+		}
+
+		public BigDecimal getValorFrete() {
+			return valorFrete;
+		}
+
+		public void setValorFrete(BigDecimal valorFrete) {
+			this.valorFrete = valorFrete;
+		}
+
+		public BigDecimal getValorDesconto() {
+			return valorDesconto;
+		}
+
+		public void setValorDesconto(BigDecimal valorDesconto) {
+			this.valorDesconto = valorDesconto;
+		}
+
+		public BigDecimal getValorTotal() {
+			return valorTotal;
+		}
+
+		public void setValorTotal(BigDecimal valorTotal) {
+			this.valorTotal = valorTotal;
+		}
+
+		public StatusPedido getStatus() {
+			return status;
+		}
+
+		public void setStatus(StatusPedido status) {
+			this.status = status;
+		}
+
+		public FormaPagamento getFormaPagamento() {
+			return formaPagamento;
+		}
+
+		public void setFormaPagamento(FormaPagamento formaPagamento) {
+			this.formaPagamento = formaPagamento;
+		}
+
+		public Usuario getVendedor() {
+			return vendedor;
+		}
+
+		public void setVendedor(Usuario vendedor) {
+			this.vendedor = vendedor;
+		}
+
+		public Cliente getCliente() {
+			return cliente;
+		}
+
+		public void setCliente(Cliente cliente) {
+			this.cliente = cliente;
+		}
+
+		public EnderecoEntrega getEnderecoEntrega() {
+			return enderecoEntrega;
+		}
+
+		public void setEnderecoEntrega(EnderecoEntrega enderecoEntrega) {
+			this.enderecoEntrega = enderecoEntrega;
+		}
+
+		public List<ItemPedido> getItens() {
+			return itens;
+		}
+
+		public void setItens(List<ItemPedido> itens) {
+			this.itens = itens;
+		}
+		
+		public int getNumeroParcela() {
+			return numeroParcela;
+		}
+
+		public void setNumeroParcela(int numeroParcela) {
+			this.numeroParcela = numeroParcela;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
